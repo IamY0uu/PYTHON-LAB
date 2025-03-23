c0ebb53cf97f398f0d32a51d1592cdabe7607002
HEMANT SINGH NEGI <168597144+IamY0uu@users.noreply.github.com>
2025-03-23 20:48:16 +0530

    Update file_by_with.py

--- file_system/file_by_with.py
+++ file_system/file_by_with.py
@@ -1,9 +1,9 @@
-# f = open("file.txt")
-# print(f.read())
-# f.close()
+ f = open("file.txt")
+ print(f.read())
+ f.close()
 
-# same can be written using "with statement" like this: 
+ same can be written using "with statement" like this: 
 
-# with open("file.txt") as f:
-#     print(f.read())
+ with open("file.txt") as f:
+     print(f.read())
     
